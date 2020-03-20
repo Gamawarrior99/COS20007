7ba67133233c8eac791e4dd4c5c659d2528a1f7c
Gamaliel D'mello <gamalieldmello99@gmail.com>
2020-03-20 23:50:19 +1100

    Add files via upload

---
+++ Week1/1.1/week1task1/week1task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace week1task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            message[] message = new message[5];
+            message[0] = new message("Welcome back oh great educator!");
+            message[1] = new message("What a lovely name");
+            message[2] = new message("Great name");
+            message[3] = new message("This name is God!");
+            message[4] = new message("That is a silly name");
+
+            Console.Write("Enter your name: ");
+
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "shamara")
+            {
+                message[0].print(); //calls the print function in the Message Class
+            }
+            else if (name.ToLower() == "john")
+            {
+                message[1].print();
+            }
+            else if (name.ToLower() == "keane")
+            {
+                message[2].print();
+            }
+            else if (name.ToLower() == "gam")
+            {
+                message[3].print();
+            }
+            else
+            {
+                message[4].print();
+            }
+        }
+    }
+}
---
+++ Week1/1.1/week1task1/week1task1/message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace week1task1
+{
+    class message
+    {
+        private string _text;
+
+        public message(String txt)
+        {
+            _text = txt;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(_text);
+        }
+    }
+}
+
