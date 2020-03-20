a7025baddf19bb868f2fead0eec295df74e89cee
Gamaliel D'mello <gamalieldmello99@gmail.com>
2020-03-20 23:52:37 +1100

    Add files via upload

---
+++ Week2/2.2/ShapeDrawer/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Window("Shape Drawer", 800, 600);
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+                if (SplashKit.MouseClicked(MouseButton.LeftButton) == true)
+                {
+                    Shape myshape = new Shape(SplashKit.MousePosition());
+                }
+
+
+
+                SplashKit.RefreshScreen();
+            } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
+
+        }
+    }
+}
